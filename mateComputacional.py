bc46e77b56e0ffe99829eaa755825dca58637c3a
MathiasHv18 <mathiashv2014@gmail.com>
2023-08-30 14:36:40 -0500

    Archivo .py

---
+++ mateComputacional.py
@@ -0,0 +1,13 @@
+
+#Matriz adjacencia?
+matrizAd = []
+
+dimensiones = int(input('Indique las dimensiones de la matriz a generar: '))
+#opcionGenerar = int(input('Desea generar los valores de manera manual o aleatoria?: '))
+
+for i in range(dimensiones):
+    for j in range(dimensiones):
+        matrizAd.append([])
+
+
+print(matrizAd)
