0dc2be6e1e967947c42202897ade8b49a77f9f1f
MathiasHv18 <mathiashv2014@gmail.com>
2023-08-30 15:24:06 -0500

    Generar matriz adjacencia

--- mateComputacional.py
+++ mateComputacional.py
@@ -1,3 +1,4 @@
+import random
 
 """
 Matriz adjacencia ejemplo
@@ -12,14 +13,31 @@ Se lee de 'A' hacia 'B'. Primero el horizontal y luego el vertical
 Cada valor es el peso que toma de X lugar a Y lugar
 En caso haya un 0 es porque no hay conexion directa entre ambas aristas
 """
-matrizAd = []
-
-dimensiones = int(input('Indique las dimensiones de la matriz a generar: '))
-#opcionGenerar = int(input('Desea generar los valores de manera manual o aleatoria?: '))
-
-for i in range(dimensiones):
-    for j in range(dimensiones):
-        matrizAd.append([])
 
 
+def generarMatriz():
+    matrizAd = []
+
+    dimensiones = int(input('Indique las dimensiones de la matriz a generar: '))
+    #Crea una matriz de X*X dimensiones con valor 0 en cada casilla    
+    for i in range(dimensiones):
+        matrizAd.append([0] * dimensiones )
+
+    opcionGenerar = input('Desea generar los valores de manera manual o aleatoria? M = Manual / A = Aleatoria: ')
+    
+    if opcionGenerar.upper() == 'M':
+        for i in range(dimensiones):
+            print('Ingrese los valores de la fila numero ' + str(i) + ': ')
+            for j in range(dimensiones):
+                valor = input('Casilla ' + str(j) + ' de ' + str(dimensiones) + ': ')
+                matrizAd[i][j] = int(valor)
+    else:
+       for i in range(dimensiones):
+           for j in range(dimensiones):
+               matrizAd[i][j] = random.randint(0,20)
+
+    return matrizAd
+
+
+matrizAd = generarMatriz()
 print(matrizAd)
